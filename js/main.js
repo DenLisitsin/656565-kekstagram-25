2e49f82bd8ffdc1b100d18d3b22a757e412d0d4e
Denis_Lisitsin <dlisitsin.noosfera@yandex.ru>
2022-03-13 15:16:09 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,16 +1,47 @@
-function getCommentLength(commentLine, maxLeigth) {
-  return commentLine.length < maxLeigth;
+const getRandomNumber = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const id = Array.from({length: 25}, (a, b) => b+1);
+const url = [];
+for (let i = 1; i < 25; i++) {
+  const str =`photos/${  i  }.jpg`;
+  url.push(str);
 }
+const description = ['Я отстаю!!!','Скоро лето, скоро вэйк','горы и сноуборд','прыгаю с парашютом','Работаю работу','учу js','мой котЭ туповат'];
+const likes = Array.from({length: 200}, (a, b) => b+15);
 
-getCommentLength ('я текст', 140);
+const getRandomArrayIndex = (element) => element[getRandomNumber(0, element.length - 1)];
 
-function getRandomNumber(min, max) {
-  if (min <= 0 && min < max) {
-    // eslint-disable-next-line no-console
-    console.log('неверное значение');
-    return;
-  }
-  return Math.floor(Math.random() * (max - min) + min);
+const userId = getRandomNumber(1, 100);
+const avatar = [];
+for (let i = 1; i < 6; i++) {
+  const str = `img/avatar-${  i  }.svg`;
+  avatar.push(str);
 }
+const userName = ['Абрам','Аваз','Август','Авдей','Автандил','Адам','Адис','Адольф','Адриан'];
+const message = ['Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+const comments = () => ({
+  id : userId,
+  avatar : getRandomArrayIndex(avatar),
+  name : getRandomArrayIndex(userName),
+  message : getRandomArrayIndex(message)
+});
 
-getRandomNumber (0, 140);
+const createObject = () => ({
+  id : getRandomArrayIndex(id),
+  url : getRandomArrayIndex(url),
+  description : getRandomArrayIndex(description),
+  likes : getRandomArrayIndex(likes),
+  comments : comments()
+});
+
+const createArray = Array.from({length: 5}, createObject);
